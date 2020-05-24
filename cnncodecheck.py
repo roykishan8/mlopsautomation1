e68bf50506da17e30d3d9bf27b9f8794053fe61d
roykishan8 <35493574+roykishan8@users.noreply.github.com>
2020-05-24 10:00:05 +0530

    test

--- cnncodecheck.py
+++ cnncodecheck.py
@@ -1,10 +1,10 @@
-programfile = open('/home/roykishan/ws/mlops1/program.py','r')	#connecting to the code file
+programfile = open('/home/roykishan/task3/mlopsautomation1/maincnncode.py','r')	#connecting to the code file
 code = programfile.read()				#reading the code file
 
 if 'keras' or 'tensorflow' in code:						#because keras or tensorflow keyword is a cmust have for a dl program
 	if 'Conv2D' in code:				#beacuse if a code is of CNN conv2D layer is a must have
-		print('kerasCNN')
+		print('CNNcode')
 	else:
-		print('not kerasCNN')
+		print('not CNNcode')
 else:
-	print('not deep learning')
+	print('not deep learning model')
